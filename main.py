97b6d9db16b1f4dc59c661d49fc69cce2ead8509
Bernardo Lopes Leão <bernardolopes@id.uff.br>
2016-12-04 17:39:57 -0200

    Captura de erro caso o usuário venha a digitar algo que não seja um número em certas opções.

--- main.py
+++ main.py
@@ -27,24 +27,30 @@ if __name__ == "__main__":
 				contador = contador + 1
 				print(contador, "-", opcao)
 			print("\nEscolha uma opção, de 1 a 5:")
-			numero = int(input())
-			if (1 <= numero <= 5):
-				escolha = opcoes[numero - 1]
-			else:
-				print("\nEscolha de 1 a 5!\n")
+			try:
+				numero = int(input())
+				if (1 <= numero <= 5):
+					escolha = opcoes[numero - 1]
+				else:
+					print("\nEscolha de 1 a 5!\n")
+			except ValueError:
+				print("\nIsso não é um número!\n")
 		meio_envio = None
 		while(meio_envio is None):
 			print("Como deseja receber a senha?")
 			print("1 - Telefone:", aluno.telefone)
 			print("2 - E-mail:", aluno.email)
 			print("\nEscolha uma opção, 1 ou 2:")
-			numero = int(input())
-			if (numero == 1):
-				meio_envio = csms(aluno.telefone)
-			elif (numero == 2):
-				meio_envio = cemail(aluno.email)
-			else:
-				print("\nEscolha 1 ou 2!\n")
+			try:
+				numero = int(input())
+				if (numero == 1):
+					meio_envio = csms(aluno.telefone)
+				elif (numero == 2):
+					meio_envio = cemail(aluno.email)
+				else:
+					print("\nEscolha 1 ou 2!\n")
+			except ValueError:
+				print("\nIsso não é um número!\n")
 		gerador.cadastrar_uffmail(aluno.matricula, aluno.nome, escolha, meio_envio)
 		print("Senha enviada à opção escolhida.")
 		aluno.uffmail = escolha
